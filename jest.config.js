61af82365e99c4de38b363551455025d00840ef5
Georgi Parlakov <gparlakov@gmail.com>
2019-09-12 22:38:46 +0300

    add a bit of optimisation - faster start up and run

--- jest.config.js
+++ jest.config.js
@@ -9,8 +9,9 @@ module.exports = {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
     '@env': '<rootDir>/src/environments/environment',
     '@src/(.*)': '<rootDir>/src/src/$1',
-    '@state/(.*)': '<rootDir>/src/app/state/$1'
-    ,'autoSpy':'<rootDir>/src/app/auto-spy.ts'
+    '@state/(.*)': '<rootDir>/src/app/state/$1',
+    autoSpy: '<rootDir>/src/app/auto-spy.ts'
   },
-  transformIgnorePatterns: ['node_modules/(?!(jest-test))']
+  transformIgnorePatterns: ['node_modules/(?!(jest-test))'],
+  maxWorkers: 4
 };
