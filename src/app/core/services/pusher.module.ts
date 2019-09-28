b7dcc396308919592b5510c42a6a9aaee1b97ae0
Georgi Parlakov <gparlakov@gmail.com>
2019-09-28 18:17:17 +0300

    stabilize the pusher provider

--- src/app/core/services/pusher.module.ts
+++ src/app/core/services/pusher.module.ts
@@ -1,24 +1,32 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule, ModuleWithProviders, InjectionToken } from '@angular/core';
 
 import { Config } from 'pusher-js';
 import { PusherService } from './pusher.service';
 
-@NgModule({})
+export const pusherKey = new InjectionToken('pusher_key');
+export const pusherConfig = new InjectionToken('pusher_config');
+
+export function pusherFactory(key: string, config: Config) {
+  return () => new PusherService(key, config);
+}
+
+@NgModule({
+  providers: []
+})
 export class PusherModule {
   static forRoot(key: string, config?: Config): ModuleWithProviders {
-    const pusherKey = 'pusher_key';
-    const pusherConfig = 'pusher_config';
     return {
       ngModule: PusherModule,
       providers: [
+        {
+          provide: PusherService,
+          useFactory: pusherFactory,
+          deps: [pusherKey, pusherConfig]
+        },
         { provide: pusherKey, useValue: key },
-        { provide: pusherConfig, useValue: config },
-        { provide: PusherService, useFactory: pusherFactory, deps: [pusherKey, pusherConfig] }
+        { provide: pusherConfig, useValue: config }
       ]
     };
   }
 }
 
-export function pusherFactory(key: string, config: Config) {
-  return new PusherService(key, config);
-}
