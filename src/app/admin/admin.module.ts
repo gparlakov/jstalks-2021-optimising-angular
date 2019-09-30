ac80d6c9f8f435b3d082b697d29b8e0e628a9861
Georgi Parlakov <gparlakov@gmail.com>
2019-09-30 20:01:46 +0300

    Add components and service in admin area

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { ArticleComponent } from './components/article/article.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, ArticleListComponent, ArticleComponent, SearchComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
